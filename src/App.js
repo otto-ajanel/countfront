5cb20b3b75790645bd03d8ec1ea5d30ca43f1539
Otto Ajanel <ottonielajanel@gmail.com>
2022-07-27 12:43:41 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="red-3">
       <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+      <p className='lime-8'>lorem</p>
+
     </div>
   );
 }
